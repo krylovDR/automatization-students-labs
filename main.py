0984a376ede7c0b72e429310ffdc84c0044544ed
krylovDR <daniil12244892@mail.ru>
2022-06-06 17:46:06 +0300

    parser_linter version 1

--- main.py
+++ main.py
@@ -1,60 +1,5 @@
-import sys
-
 import csv
 import gspread
-import cpplint
-
-
-class ErrorCollector(object):
-    _ERROR_CATEGORIES = cpplint._ERROR_CATEGORIES
-    _SEEN_ERROR_CATEGORIES = {}
-
-    def __init__(self):
-        self._errors = []
-        cpplint.ResetNolintSuppressions()
-
-    def __call__(self, unused_filename, linenum,
-                 category, confidence, message):
-        self._SEEN_ERROR_CATEGORIES[category] = 1
-        if cpplint._ShouldPrintError(category, confidence, linenum):
-            if confidence > 3:
-                self._errors.append('%d: %s  [%s] [%d]' % (linenum + 1, message, category, confidence))
-
-    def results(self):
-        if len(self._errors) < 2:
-            return ''.join(self._errors)  # Most tests expect to have a string.
-        else:
-            return self._errors  # Let's give a list if there is more than one.
-
-    def result_list(self):
-        return self._errors
-
-    def verify_all_categories_are_seen(self):
-        for category in self._ERROR_CATEGORIES:
-            if category not in self._SEEN_ERROR_CATEGORIES:
-                sys.exit('FATAL ERROR: There are no tests for category "%s"' % category)
-
-    def remove_if_present(self, substr):
-        for (index, error) in enumerate(self._errors):
-            if error.find(substr) != -1:
-                self._errors = self._errors[0:index] + self._errors[(index + 1):]
-                break
-
-
-def perform_multiline_lint(code):
-    error_collector = ErrorCollector()
-    lines = code.split('\n')
-    cpplint.RemoveMultiLineComments('main.cpp', lines, error_collector)
-    lines = cpplint.CleansedLines(lines)
-    nesting_state = cpplint.NestingState()
-
-    for i in range(lines.NumLines()):
-        nesting_state.Update('main.cpp', lines, i, error_collector)
-        cpplint.CheckStyle('main.cpp', lines, i, 'cpp', nesting_state, error_collector)
-        cpplint.CheckForNonStandardConstructs('main.cpp', lines, i, nesting_state, error_collector)
-    nesting_state.CheckCompletedBlocks('main.cpp', error_collector)
-
-    return error_collector.result_list()
 
 
 sa = gspread.service_account(filename="credentials.json")  # используем ключ сервисного аккаунта
@@ -125,21 +70,6 @@ with open("2.csv", encoding='utf-8') as r_file:
                 print("flag 8")
             elif float(marks[3][student_number][0].replace(',', '.')) < 0.39:
                 marks[3][student_number] = [str(float(marks[3][student_number][0].replace(',', '.')) + 0.4)]
-
-
-# обработка и учёт результатов линтера (Иванов 3 лаба)
-st_num_lint = 196  # студент Иванов (демо)
-
-f = open('main_no_err.cpp', 'r')
-errors = perform_multiline_lint(f.read())
-
-# если лаба сдана, то прибавить 0.4 балла за тесты, если уже проставлено максимум за оформление
-if len(errors) == 0:
-    if marks[2][st_num_lint] == []:
-        marks[2][st_num_lint] = [str(0.3)]
-    elif 0.31 < float(marks[2][st_num_lint][0].replace(',', '.')) < 0.69:
-        marks[2][st_num_lint] = [str(float(marks[2][st_num_lint][0].replace(',', '.')) + 0.3)]
-
 
 # обновление данных в таблице
 wks.update('D5:D201', marks[0])  # 0 лаба
